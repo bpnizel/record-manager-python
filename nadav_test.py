4a3085482cc26ada74ceea30e8ee6f9b3ab0a576
bpnizel <155309205+bpnizel@users.noreply.github.com>
2026-02-10 20:14:44 +0200

    Update nadav_test.py

--- nadav_test.py
+++ nadav_test.py
@@ -1,15 +1,1 @@
-def main():
-    foo()
 
-def foo():
-    bar()
-
-def bar():
-    raise ZeroDivisionError("Hello")
-
-
-try:
-    main()
-except ZeroDivisionError as e:
-    print("Hey, i cought it")
-    print(e)

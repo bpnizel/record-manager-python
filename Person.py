dd37ce462a921a0906a2fcb750d27a4879023aa3
bpnizel <155309205+bpnizel@users.noreply.github.com>
2026-07-14 15:04:13 +0300

    Update Person.py

--- Person.py
+++ Person.py
@@ -29,23 +29,4 @@ class Person: # The first letter  in class Upper
 if __name__ == "__main__":
     print("Error: This file should not be running. this is a class file")
 
-# try:
-#     if __name__ == "__main__":
-#         person_1 = Person("12345")
-#         print(person_1) 
-# except Exception as e:
-#     print(e) 
-    
-#     test_id = "1234" 
-#     test_name = "test_name"
-#     test_age = 90 
 
-#     person = Person( test_id)
-#     print(person) 
-#     if person.getId() != test_id:
-#         print("Error: Age should be " + test_id + " but i got " + person.getId())
-#     if person.getName() != test_name:
-#         print("Error: Name should be " + test_name + " but i got " + person.getName())
-#     if person.getAge() != test_age:
-#         print("Error: Height should be " + str(test_age) + " but i got " + str(person.getAge()))
-     
